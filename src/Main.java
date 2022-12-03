0dafa891f39ad7d377bf4e1da9bec2b55df36ba3
Sergei Gridnev <sergeigr32@gmail.com>
2022-12-03 18:14:25 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public final static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     static int max = 0;
     static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
         Map<Character, Integer> map = new HashMap<>();
         String newText = text.toLowerCase();
@@ -24,30 +25,29 @@ public class Main {
             }
         }
 
-        System.out.println(map);
-
-        System.out.println(maxValue(map));
-        System.out.println(minValue(map));
-
-
+        System.out.println("Наиболлее употребляемый символ - " + maxValue(map) + ", а наименее употребляемый - " + minValue(map));
     }
 
-    private static int maxValue(Map<Character, Integer> map){
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-            if(entry.getValue() > max){
-                max =entry.getValue();
-            }
-        }
-        return max;
-    }
-
-    private static int minValue(Map<Character, Integer> map){
+    private static char maxValue(Map<Character, Integer> map) {
+        char maxEntry = ' ';
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if(entry.getValue() < min){
-                min =entry.getValue();
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                maxEntry = entry.getKey();
             }
         }
-        return min;
+        return maxEntry;
+    }
+
+    private static char minValue(Map<Character, Integer> map) {
+        char minEntry = ' ';
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() < min) {
+                min = entry.getValue();
+                minEntry = entry.getKey();
+            }
+        }
+        return minEntry;
     }
 }
 
